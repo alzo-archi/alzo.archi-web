07aa908b15619e07055b8678774b22f991bd869e
Chantepierre <lucas@lucassifoni.info>
2024-05-28 11:09:01 +0200

    Rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://relax.archi',
+  site: 'https://alzo.archi',
   integrations: [vue(), tailwind()]
 });
