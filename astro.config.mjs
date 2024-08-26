ee0bc0f6a75f79bfd752b7d12f49520ffdb3434f
Chantepierre <lucas@lucassifoni.info>
2024-08-26 17:42:28 +0200

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alzo.archi',
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  devToolbar: {
+    enabled: false
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: "#ff3300",
+				'brand-darker': "#d7370f",
+				isabelline: '#f8f6f2',
+				'isabelline-light': 'rgb(252, 251, 251)',
+				paledogwood: '#fcd4c5',
+				goldengatebridge: '#e84e0f',
+				olivine: '#aab46f',
+				darkmossgreen: '#575e31',
+				carolinablue: '#8fbee0',
+				celestialblue: '#4a97ce',
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('daisyui'),
+	],
 }
