cead69da085fcaff323510e1b168a550a7f2a629
Chantepierre <lucas@lucassifoni.info>
2024-11-30 14:23:56 +0100

    UI Fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				brand: "#ff3300",
-				'brand-darker': "#d7370f",
-				isabelline: '#f8f6f2',
-				'isabelline-light': 'rgb(252, 251, 251)',
-				paledogwood: '#fcd4c5',
-				goldengatebridge: '#e84e0f',
-				olivine: '#aab46f',
-				darkmossgreen: '#575e31',
-				carolinablue: '#8fbee0',
-				celestialblue: '#4a97ce',
-			}
-		},
-	},
-	plugins: [
-		require('daisyui'),
-	],
-	daisyui: {
-		themes: false
-	}
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ff4100",
+        "brand-darker": "#d7370f",
+        isabelline: "#f8f6f2",
+        "isabelline-light": "rgb(252, 251, 251)",
+        paledogwood: "#fcd4c5",
+        goldengatebridge: "#e84e0f",
+        olivine: "#aab46f",
+        darkmossgreen: "#575e31",
+        carolinablue: "#8fbee0",
+        celestialblue: "#4a97ce",
+      },
+    },
+    fontWeight: {
+      normal: "250",
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
+};
